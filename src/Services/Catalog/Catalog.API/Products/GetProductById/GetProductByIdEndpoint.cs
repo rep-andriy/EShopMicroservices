b4e16b8dc3a818d031fe82781767ae32bd879231
rep-andriy <andriyreplianskiy@gmail.com>
2025-08-09 16:38:14 +0300

    implemented CRUD endpoints and handlers for Product

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+//public record GetProductByIdRequest();
+public record GetProductBydIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductBydIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductBydIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
+}

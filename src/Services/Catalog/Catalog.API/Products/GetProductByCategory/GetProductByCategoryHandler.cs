c2840f3011ed4806b1a04b932dcfc2e0d441d250
rep-andriy <andriyreplianskiy@gmail.com>
2025-08-19 02:11:05 +0300

    enhanced validation, error handling, and Docker setup

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -5,13 +5,11 @@ public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCa
 public record GetProductByCategoryResult(IEnumerable<Product> Products);
 
 internal class GetProductByCategoryHandler
-    (IDocumentSession session, ILogger<GetProductByCategoryResult> logger)
+    (IDocumentSession session)
     : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
     public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
-
         var products = await session.Query<Product>()
             .Where(p => p.Category.Contains(query.Category))
             .ToListAsync();
